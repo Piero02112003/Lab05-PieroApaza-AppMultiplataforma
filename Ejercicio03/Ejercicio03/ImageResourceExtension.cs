5ba76ba7eb9b434e4a64e19221189eaea3a76e7e
Piero_Apaza <piero02112003@gmail.com>
2023-04-30 00:05:27 -0500

    first commit

---
+++ Ejercicio03/Ejercicio03/ImageResourceExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio03
+{
+    [PreserveSig(AllMembers = true)]
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public IRunningObjectTable ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+            var imageSource =ImageSource.FromResource(Source,
+                typeof(ImageSourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
